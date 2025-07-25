d3ea174e727cc2dfaafc86f3236d0e192f43dafc
kittiphiphat <makuth824@gmail.com>
2025-07-25 15:59:57 +0700

    v.0.1.2

--- src/extensions/users-permissions/strapi-server.js
+++ src/extensions/users-permissions/strapi-server.js
@@ -36,45 +36,6 @@ module.exports = (plugin) => {
     };
   };
 
-  // Override delete function
-  plugin.controllers.user.delete = async (ctx) => {
-     console.log('Extension users-permissions strapi-server loaded');
-    const admin = ctx.state.user;
-    const { id } = ctx.params;
-
-    console.log('Delete user called by:', admin?.username);
-
-    if (!admin) {
-      return ctx.unauthorized('คุณไม่ได้รับอนุญาต');
-    }
-
-    const userToDelete = await strapi.entityService.findOne('plugin::users-permissions.user', id);
-
-    if (!userToDelete) {
-      return ctx.notFound('ไม่พบผู้ใช้');
-    }
-
-    try {
-      await strapi.entityService.delete('plugin::users-permissions.user', id);
-
-      console.log('User deleted, calling adminLogHelper');
-
-      await adminLogHelper({
-        action: 'user_deleted',
-        type: 'delete',
-        message: `แอดมิน ${admin.username} ลบผู้ใช้ ${userToDelete.username}`,
-        user: { id: admin.id },
-        details: { before: userToDelete },
-      });
-
-      console.log('Log saved successfully');
-
-      return { message: 'ลบผู้ใช้เรียบร้อยแล้ว' };
-    } catch (error) {
-      console.error('❌ Error deleting user or logging:', error);
-      return ctx.internalServerError('ไม่สามารถลบผู้ใช้ได้');
-    }
-  };
 
   return plugin;
 };
